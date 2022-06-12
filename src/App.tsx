483da0ea9bf3354b5331858a9e030b76f770a237
Victor Foster <victor.foster@gmail.com>
2022-06-12 12:42:25 -0400

    Added prettier and eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
 import { Component, createSignal } from 'solid-js';
 import './App.scss';
-import Counter from './Counter';
 import { barmy } from './lyrics/barmy';
 import { behindTheCounter } from './lyrics/behind-the-counter';
 import { bloodOuttaStone } from './lyrics/blood-outta-stone';
 import { bombast } from './lyrics/bombast';
+import { brillo } from './lyrics/brillo-de-facto';
 import { bury } from './lyrics/bury';
 import { cabItUp } from './lyrics/cab-it-up';
-import { couldntGetAhead } from './lyrics/couldnt-get-ahead';
+import { getAhead } from './lyrics/couldnt-get-ahead';
 import { cruisersCreek } from './lyrics/cruisers-creek';
-import { deadbeatDescedant } from './lyrics/dead-beat-descendant';
+import { deadbeatDescendant } from './lyrics/dead-beat-descendant';
 import { distilledMugArt } from './lyrics/distilled-mug-art';
 import { flatOfAngles } from './lyrics/flat-of-angles';
-import { gibbusGibson } from './lyrics/gibbus-gibson';
-import { iFeelVoxish } from './lyrics/i-feel-voxish';
+import { gibson } from './lyrics/gibbus-gibson';
+import { iFeel } from './lyrics/i-feel-voxish';
 import { itsACurse } from './lyrics/its-a-curse';
 import { justStepSWays } from './lyrics/just-step-sways';
 import { livingTooLate } from './lyrics/living-too-late';
@@ -21,7 +21,7 @@ import { newBigPrinz } from './lyrics/new-big-prinz';
 import { paranoiaMan } from './lyrics/paranoia-man-in-cheap-shit-room';
 import { rudeAllTheTime } from './lyrics/rude-all-the-time';
 import { shoulderPads } from './lyrics/shoulder-pads';
-import { sirWilliamWray } from './lyrics/sir-william-wray';
+import { sirWilliam } from './lyrics/sir-william';
 import { takingOff } from './lyrics/taking-off';
 import { theSteakPlace } from './lyrics/the-steak-place';
 import { imTotallyWired } from './lyrics/totally-wired';
@@ -31,50 +31,79 @@ import { whiteLightning } from './lyrics/white-lightning';
 import { wiseOlMan } from './lyrics/wise-ol-man';
 
 const lyrics = [
-  barmy, behindTheCounter, bloodOuttaStone, bombast, bury, cabItUp, 
-  couldntGetAhead, cruisersCreek, deadbeatDescedant, distilledMugArt, 
-  flatOfAngles, gibbusGibson, iFeelVoxish, itsACurse, justStepSWays, livingTooLate, newBigPrinz,
-  paranoiaMan, rudeAllTheTime, shoulderPads, sirWilliamWray, takingOff,
-  theSteakPlace, imTotallyWired, victrolaTime, vixen, whiteLightning, wiseOlMan
+	barmy,
+	behindTheCounter,
+	bloodOuttaStone,
+	bombast,
+	brillo,
+	bury,
+	cabItUp,
+	getAhead,
+	cruisersCreek,
+	deadbeatDescendant,
+	distilledMugArt,
+	flatOfAngles,
+	gibson,
+	iFeel,
+	itsACurse,
+	justStepSWays,
+	livingTooLate,
+	newBigPrinz,
+	paranoiaMan,
+	rudeAllTheTime,
+	shoulderPads,
+	sirWilliam,
+	takingOff,
+	theSteakPlace,
+	imTotallyWired,
+	victrolaTime,
+	vixen,
+	whiteLightning,
+	wiseOlMan,
 ];
 
-const getRandomItem = () => {
-  const randomIndex = Math.floor(Math.random() * lyrics.length);
-  const item = lyrics[randomIndex];
-  return item;
-}
-
-const fetchLyric = async (songName) => (await fetch(`/lyrics/${songName}.md`)).json();
+const getRandomLyrics = () => {
+	const randomIndex = Math.floor(Math.random() * lyrics.length);
+	const item = lyrics[randomIndex];
+	return item;
+};
 
 const App: Component = () => {
-  const [ipsum, setIpsum] = createSignal(['']);
+	const [ipsum, setIpsum] = createSignal(['']);
 
-  return (
-    <>
-      <header>
-        <h1 class='the-fall'>The Fall</h1>
-      </header>
-      <select title="select-number-of-paragraphs" name="paragrah" id="paragrah-select" onChange={(e) => {
-        const numberOfParagraphs = parseInt(e.currentTarget.value);
-        const results = Array.from({ length: numberOfParagraphs }, () => getRandomItem());
-        setIpsum(results);
-      }}>
-        <option value="">Select paragraghs</option>
-        <option value="1">1 paragraph</option>
-        <option value="2">2 paragraphs</option>
-        <option value="3">3 paragraphs</option>
-        <option value="5">5 paragraphs</option>
-        <option value="9">9 paragraphs</option>
-        <option value="13">13 paragraphs</option>
-      </select>
-      <button>Shout!</button>
-      <div class='ipsum'>
-        {ipsum().filter(ipsum => !!ipsum).map(item => (
-          <p>{item}</p>
-        ))}
-      </div>
-    </>
-  );
+	return (
+		<>
+			<header>
+				<h1 class='the-fall'>The Fall</h1>
+			</header>
+			<select
+				title='select-number-of-paragraphs'
+				name='paragrah'
+				id='paragrah-select'
+				onChange={(e) => {
+					const numberOfParagraphs = parseInt(e.currentTarget.value, 10) ?? 1;
+					const results = Array.from({ length: numberOfParagraphs }, () => getRandomLyrics());
+					setIpsum(results);
+				}}
+			>
+				<option value=''>Select paragraghs</option>
+				<option value='1'>1 paragraph</option>
+				<option value='2'>2 paragraphs</option>
+				<option value='3'>3 paragraphs</option>
+				<option value='5'>5 paragraphs</option>
+				<option value='9'>9 paragraphs</option>
+				<option value='13'>13 paragraphs</option>
+			</select>
+			<button>Shout!</button>
+			<div class='ipsum'>
+				{ipsum()
+					.filter((ipsum) => Boolean(ipsum))
+					.map((item) => (
+						<p>{item}</p>
+					))}
+			</div>
+		</>
+	);
 };
 
 export default App;
---
+++ src/lyrics/brillo-de-facto.ts
@@ -0,0 +1,45 @@
+export const brillo = `He got no man
+Go straight
+Brillo chin
+In kitchen
+Put a dunce
+Shed
+To kick himself
+A a celestial
+Sorry
+Tiny tears of bling
+We all know
+Where he came from
+
+Check this out
+Loves him
+
+Brillo chin
+Brillo chin
+The Kennedy
+The Kennedy
+I asked her
+He's here
+I wanna tell
+How pale he is
+I said
+He's got no legs
+He got no man
+Got no de facto plan
+
+Brillo chin
+Brillo chin
+Got no plan
+Little elf
+
+Tales of Brillo
+Face down
+And the asphyxiation of the troll will finally be
+All salute at the altar of filo pastry
+At the altar of Kennedy, John
+Victim
+In Victoria Station
+Monty
+The James Fennings
+And the infants
+To suckle him`;
