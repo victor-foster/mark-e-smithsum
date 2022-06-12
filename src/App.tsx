78e5a9a35b474f964c940c26e5e6868cf401e16b
Victor Foster <victor.foster@gmail.com>
2022-06-12 14:32:33 -0400

    Cleaned up the import statements

--- src/App.tsx
+++ src/App.tsx
@@ -1,68 +1,7 @@
-import { Component, createSignal, For, Match, Switch } from 'solid-js';
+import { Component, createSignal } from 'solid-js';
 import './App.scss';
 import Header from './Header';
-import { Transition, TransitionGroup } from 'solid-transition-group';
-import { barmy } from './lyrics/barmy';
-import { behindTheCounter } from './lyrics/behind-the-counter';
-import { bloodOuttaStone } from './lyrics/blood-outta-stone';
-import { bombast } from './lyrics/bombast';
-import { brillo } from './lyrics/brillo-de-facto';
-import { bury } from './lyrics/bury';
-import { cabItUp } from './lyrics/cab-it-up';
-import { getAhead } from './lyrics/couldnt-get-ahead';
-import { cruisersCreek } from './lyrics/cruisers-creek';
-import { deadbeatDescendant } from './lyrics/dead-beat-descendant';
-import { distilledMugArt } from './lyrics/distilled-mug-art';
-import { flatOfAngles } from './lyrics/flat-of-angles';
-import { gibson } from './lyrics/gibbus-gibson';
-import { iFeel } from './lyrics/i-feel-voxish';
-import { itsACurse } from './lyrics/its-a-curse';
-import { justStepSWays } from './lyrics/just-step-sways';
-import { livingTooLate } from './lyrics/living-too-late';
-import { newBigPrinz } from './lyrics/new-big-prinz';
-import { paranoiaMan } from './lyrics/paranoia-man-in-cheap-shit-room';
-import { rudeAllTheTime } from './lyrics/rude-all-the-time';
-import { shoulderPads } from './lyrics/shoulder-pads';
-import { sirWilliam } from './lyrics/sir-william';
-import { takingOff } from './lyrics/taking-off';
-import { theSteakPlace } from './lyrics/the-steak-place';
-import { imTotallyWired } from './lyrics/totally-wired';
-import { victrolaTime } from './lyrics/victrola-time';
-import { vixen } from './lyrics/vixen';
-import { whiteLightning } from './lyrics/white-lightning';
-import { wiseOlMan } from './lyrics/wise-ol-man';
-
-const lyrics = [
-	barmy,
-	behindTheCounter,
-	bloodOuttaStone,
-	bombast,
-	brillo,
-	bury,
-	cabItUp,
-	getAhead,
-	cruisersCreek,
-	deadbeatDescendant,
-	distilledMugArt,
-	flatOfAngles,
-	gibson,
-	iFeel,
-	itsACurse,
-	justStepSWays,
-	livingTooLate,
-	newBigPrinz,
-	paranoiaMan,
-	rudeAllTheTime,
-	shoulderPads,
-	sirWilliam,
-	takingOff,
-	theSteakPlace,
-	imTotallyWired,
-	victrolaTime,
-	vixen,
-	whiteLightning,
-	wiseOlMan,
-];
+import { lyrics } from './lyrics';
 
 const getRandomLyrics = () => {
 	const randomIndex = Math.floor(Math.random() * lyrics.length);
---
+++ src/lyrics/index.ts
@@ -0,0 +1,61 @@
+import { barmy } from './barmy';
+import { behindTheCounter } from './behind-the-counter';
+import { bloodOuttaStone } from './blood-outta-stone';
+import { bombast } from './bombast';
+import { brillo } from './brillo-de-facto';
+import { bury } from './bury';
+import { cabItUp } from './cab-it-up';
+import { getAhead } from './couldnt-get-ahead';
+import { cruisersCreek } from './cruisers-creek';
+import { deadbeatDescendant } from './dead-beat-descendant';
+import { distilledMugArt } from './distilled-mug-art';
+import { flatOfAngles } from './flat-of-angles';
+import { gibson } from './gibbus-gibson';
+import { iFeel } from './i-feel-voxish';
+import { itsACurse } from './its-a-curse';
+import { justStepSWays } from './just-step-sways';
+import { livingTooLate } from './living-too-late';
+import { newBigPrinz } from './new-big-prinz';
+import { paranoiaMan } from './paranoia-man-in-cheap-shit-room';
+import { rudeAllTheTime } from './rude-all-the-time';
+import { shoulderPads } from './shoulder-pads';
+import { sirWilliam } from './sir-william';
+import { takingOff } from './taking-off';
+import { theSteakPlace } from './the-steak-place';
+import { imTotallyWired } from './totally-wired';
+import { victrolaTime } from './victrola-time';
+import { vixen } from './vixen';
+import { whiteLightning } from './white-lightning';
+import { wiseOlMan } from './wise-ol-man';
+
+export const lyrics = [
+	barmy,
+	behindTheCounter,
+	bloodOuttaStone,
+	bombast,
+	brillo,
+	bury,
+	cabItUp,
+	getAhead,
+	cruisersCreek,
+	deadbeatDescendant,
+	distilledMugArt,
+	flatOfAngles,
+	gibson,
+	iFeel,
+	itsACurse,
+	justStepSWays,
+	livingTooLate,
+	newBigPrinz,
+	paranoiaMan,
+	rudeAllTheTime,
+	shoulderPads,
+	sirWilliam,
+	takingOff,
+	theSteakPlace,
+	imTotallyWired,
+	victrolaTime,
+	vixen,
+	whiteLightning,
+	wiseOlMan,
+];
